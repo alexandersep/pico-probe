94d1fbe3738d05c0b5a561e81d29151b60dc835b
Alex <71922720+alexandersep@users.noreply.github.com>
2022-07-24 23:49:03 +0100

    Cleaned up vscode extension section
    Changed to for loop, for shorter code. Removed unnecessary condition checking and reduced it. Fixed bug, Where extensions were not properly installing due due bash syntax error. Changed if statements to use double square brackets to better suit the bash syntax

--- pico_setup.sh
+++ pico_setup.sh
@@ -6,7 +6,7 @@ set -e
 # Number of cores when running make (default number of threads you have, can be replaced with a number (not in quotation marks))
 #example 
 #JNUM=4
-JNUM="$(grep -c processor /proc/cpuinfo)"
+JNUM=`nproc`
 
 # Where will the output go, (YOUR_CURRENT_LOCATION/pico) e.g. /home/user/Downloads/pico (if current location is Downloads)
 OUTDIR="$(pwd)/pico"
@@ -44,7 +44,7 @@ cd
 SDK_IN_BASHRC=$(cat .bashrc | grep "PICO_SDK_PATH" | cut -d ' ' -f 2 | cut -d '=' -f 1)
 cd $OUTDIR
 
-if [ -d $DEST ]; then 
+if [[ -d $DEST ]]; then 
 	echo "$DEST already exists so skipping"
 else
 	REPO_URL="${GITHUB_PREFIX}pico-${REPO}${GIT_SUFFIX}"
@@ -59,7 +59,7 @@ else
 	# Define PICO_SDK_PATH in ~/.bashrc
 	VARNAME="PICO_${REPO^^}_PATH"
 	echo "Adding $VARNAME to ~/.bashrc"
-	if [ "$SDK_IN_BASHRC" != "" ]; then
+	if [[ "$SDK_IN_BASHRC" != "" ]]; then
 		echo "$SDK_IN_BASHRC has been found in the bashrc file so skipping"
 	else
 		echo "export $VARNAME=$DEST" >> ~/.bashrc
@@ -75,7 +75,7 @@ cd $OUTDIR
 for REPO in picoprobe picotool
 do
     DEST="$OUTDIR/$REPO"
-    if [ -d "$OUTDIR/$REPO" ]; then 
+    if [[ -d "$OUTDIR/$REPO" ]]; then 
 	    echo "$REPO already exists skipping"
     else 
 	    REPO_URL="${GITHUB_PREFIX}${REPO}${GITHUB_SUFFIX}"
@@ -92,9 +92,9 @@ do
 		echo "Attempting to install picotool to /usr/local/bin/picotool"
 		DEST_LOCAL="/usr/local/bin/picotool"
 		DEST="/usr/bin/picotool"
-		if [ -e $DEST_LOCAL ]; then 
+		if [[ -e $DEST_LOCAL ]]; then 
 			echo "picotool already exists and can be found in $DEST_LOCAL"
-		elif [ -e $DEST ]; then
+		elif [[ -e $DEST ]]; then
 			echo "picotool already exists and can be found in $DEST"
 		else
 			sudo cp picotool /usr/local/bin/
@@ -127,7 +127,7 @@ cd $OUTDIR
 echo "Attempting to install arm-none-eabi-gdb"
 GCC_ARM="arm-none-eabi.tar.bz2"
 GCC_BIN_LOCAL_EXISTS="/usr/local/bin/arm-none-eabi-gdb"
-if  [ -e $GCC_BIN_LOCAL_EXISTS ]; then
+if  [[ -e $GCC_BIN_LOCAL_EXISTS ]]; then
 	echo "Binary already installed it is found in $GCC_BIN_LOCAL_EXISTS"
 else
 	echo "Installing gcc-arm-none-eabi"
@@ -147,7 +147,7 @@ fi
 echo "Attempting to give permissions to plugdev group for openocd debugging"
 OPENOCD_RULES_60="/etc/udev/rules.d/60-openocd.rules"
 OPENOCD_RULES_98="/etc/udev/rules.d/98-openocd.rules"
-if [ -e $OPENOCD_RULES_60 ]; then
+if [[ -e $OPENOCD_RULES_60 ]]; then
 	echo "$OPENOCD_RULES_60 already exists, assuming it's configured already"	
 else
 	echo "Raspberry Pi Picoprobe" | sudo tee --append $OPENOCD_RULES_60
@@ -158,7 +158,7 @@ else
 fi
 
 # This may not be needed
-if [ -e $OPENOCD_RULES_98 ]; then
+if [[ -e $OPENOCD_RULES_98 ]]; then
 	echo "$OPENOCD_RULES_98 already exists, assuming it's configured already"
 else
 	echo "ACTION!=\"add|change\", GOTO=\"openocd_rules_end\"" | sudo tee --append $OPENOCD_RULES_98
@@ -180,16 +180,16 @@ echo
 echo "Preparing installation of vscode"
 VSCODE_LOCATION="/usr/share/code/"
 VSCODE="code"
-if [ -f vscode.deb ]; then
+if [[ -f vscode.deb ]]; then
         echo "Skipping vscode as vscode.deb exists"
 	VSCODE="code"
-elif [ -d  $VSCODE_LOCATION ]; then
+elif [[ -d  $VSCODE_LOCATION ]]; then
 	echo "Skipping vscode" 
 	echo "Make sure you have these extensions installed"
 	echo "marus25.cortex-debug"
 	echo "ms-vscode.cmake-tools"
 	echo "ms-vscode.cpptools"
-elif [ "$ANSWER" == "y" ] || [ "$ANSWER" == "yes" ]; then
+elif [[ "$ANSWER" == "y" ]] || [[ "$ANSWER" == "yes" ]]; then
    	echo "Installing VSCODE"
 	VSCODE_DEB="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 
@@ -210,64 +210,31 @@ echo "----------------------------------"
 EX=("dan-c-underwood.arm" "jeff-hykin.better-cpp-syntax" "ms-vscode.cpptools" "ms-vscode.cpptools-extension-pack"
     "ms-vscode.cpptools-themes" "twxs.cmake" "marus25.cortex-debug" "cschlosser.doxdocgen" "betwo.vscode-doxygen-runner"
     "betwo.vscode-doxygen-runner" "mhutchie.git-graph" "eamodio.gitlens")
-echo "1.${EX[0]}"
-echo "2.${EX[1]}"
-echo "3.${EX[2]}"
-echo "4.${EX[3]}"
-echo "5.${EX[4]}"
-echo "6.${EX[5]}"
-echo "7.${EX[6]}"
-echo "8.${EX[7]}"
-echo "9.${EX[8]}"
-echo "10.${EX[9]}"
-echo "11.${EX[10]}"
+
+for num in {1..11}
+do
+	echo "$num.${EX[$((num-1))]}"
+done
 
 read -p "Do you want to install the some or all extensions that are given in the module lectures? y/n: " ANSWER
 echo
-if [ "$ANSWER" == "n" ] || [ "$ANSWER" == "no" ]; then
+if [[ "$ANSWER" == "n" ]] || [[ "$ANSWER" == "no" ]]; then
 	echo "You have selected no to installing the extensions"
-elif [ "$ANSWER" == "y" ] || [ "$ANSWER" == "yes" ]; then
+elif [[ "$ANSWER" == "y" ]] || [[ "$ANSWER" == "yes" ]]; then
 	echo "You have selected yes"
 	read -p "What extension do you want to install? (type exit to stop, or list to list the extensions again): " ANSWER
 	echo 
-	while [ "$ANSWER" != "exit" ];
+	while [[ "$ANSWER" != "exit" ]];
 	do
 		# List of extensions that we were told to install (if already installed, it will skip unless specify --force which I don't)
 		# 
-		if [ "$ANSWER" == "list" ]; then
-			echo "1.${EX[0]}"
-			echo "2.${EX[1]}"
-			echo "3.${EX[2]}"
-			echo "4.${EX[3]}"
-			echo "5.${EX[4]}"
-			echo "6.${EX[5]}"
-			echo "7.${EX[6]}"
-			echo "8.${EX[7]}"
-			echo "9.${EX[8]}"
-			echo "10.${EX[9]}"
-			echo "11.${EX[10]}"
-		elif [ "$ANSWER" == "1" ]; then
-			$VSCODE --install-extension $EX[0] 
-		elif [ "$ANSWER" == "2" ]; then 
-			$VSCODE --install-extension $EX[1]
-		elif [ "$ANSWER" == "3" ]; then 
-			$VSCODE --install-extension $EX[2]
-		elif [ "$ANSWER" == "4" ]; then 
-			$VSCODE --install-extension $EX[3]
-		elif [ "$ANSWER" == "5" ]; then 
-			$VSCODE --install-extension $EX[4]
-		elif [ "$ANSWER" == "6" ]; then 
-			$VSCODE --install-extension $EX[5]
-		elif [ "$ANSWER" == "7" ]; then 
-			$VSCODE --install-extension $EX[6]
-		elif [ "$ANSWER" == "8" ]; then 
-			$VSCODE --install-extension $EX[7]
-		elif [ "$ANSWER" == "9" ]; then 
-			$VSCODE --install-extension $EX[8]
-		elif [ "$ANSWER" == "10" ]; then 
-			$VSCODE --install-extension $EX[9]
-		elif [ "$ANSWER" == "11" ]; then 
-			$VSCODE --install-extension $EX[10]
+		if [[ "$ANSWER" == "list" ]]; then
+			for num in {1..11}
+			do
+				echo "$num.${EX[$((num-1))]}"
+			done
+		elif [[ "$ANSWER" -ge "1" ]] && [[ "$ANSWER" -le "11" ]]; then
+			$VSCODE --install-extension "${EX[$((ANSWER-1))]}" 
 		else
 			echo "Error: try again, input a number betwen 1-11"
 		fi
